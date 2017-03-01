d4848180222eef050865b131f7ac89227e8b77f7
ShaAli <sali1@stuy.edu>
2017-03-01 07:19:04 -0500

    changes

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 matrix = new_matrix()
+m2 = new_matrix()
+add_edge(matrix,1,2,3,4,5,6)
+add_edge(matrix,7,8,9,10,11,12)
+add_edge(m2,1,2,3,4,5,6)
+add_edge(m2,7,8,9,10,11,12)
+print("Original matrix:")
+print_matrix(matrix)
+print("Testing matrix_mult, multiply matrix by itself:\n")
+print_matrix(matrix_mult(matrix,m2))
+print("Testing ident():\n")
+ident(matrix)
+print_matrix(matrix)
+print("Testing scalar_mult():\nMultiply matrix by 10:\n")
+scalar_mult(matrix,10)
+print_matrix(matrix)
 
 
 draw_lines( matrix, screen, color )
--- matrix.py
+++ matrix.py
@@ -2,19 +2,38 @@ import math
 
 
 def print_matrix( matrix ):
-    pass
+    str = ""
+    for r in range(len(matrix)):
+        for c in range(len(matrix[r])):
+            str += matrix[r][c]
+            str += "\t"
+        str += "\n"
+    print str
 
 def ident( matrix ):
-    pass
+    for r in range(len(matrix)):
+        for c in range(len(matrix[r])):
+            if r==c:
+                matrix[r][c] = 1
+            else:
+                 matrix[r][c] = 0
+    return matrix
+    
 
 def scalar_mult( matrix, s ):
-    pass
+    for r in range(len(matrix)):
+        for c in range(len(matrix[r])):
+            matrix[r][c] = matrix[r][c]*s
+    return matrix
 
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
-    pass
-
-
+    tmp = new_matrix(len(m2[0], len(m1))
+    for r in range(len(m1)):#this is throwing a syntax error!? I'm not sure if I am missing something obvious.
+        for c in range(len(m2[0])):
+            for i in range(len(m2)):
+                     tmp[r][c] += m1[r][i]*m2[i][c]
+    return tmp
 
 
 def new_matrix(rows = 4, cols = 4):
